74e95d0126acab44faf018b13ab4fbd234677b66
shfana <wambitafana@gmail.com>
2024-05-21 15:57:51 +0300

    Correct error message

--- asciiart/main.go
+++ asciiart/main.go
@@ -9,20 +9,23 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 1 || len(os.Args) > 5 {
-		fmt.Println("Usage: go run . --color=<color> <letters to be colored> \"something\" --bannerfile")
+	// check length of arguments
+	if len(os.Args) == 1 || len(os.Args) > 5 {
+		fmt.Println("Usage: go run . [OPTION] [STRING] \n\nEX: go run . --color=<color> <letters to be colored> \"something\"")
 		return
 	}
-	var inputString string
-	var lettersToColor string
-	var filePath string
-	// var colorFlag string
-	var color string
+	var (
+		inputString    string
+		lettersToColor string
+		color          string
+	)
+	// case: no colors selected
 	if len(os.Args) == 2 {
 		inputString = os.Args[1]
 		lettersToColor = inputString
 		color = "resetCode"
 	}
+	// case color selected but no specified letters to be colored
 	if len(os.Args) == 3 {
 		colorFlag := os.Args[1]
 		inputString = os.Args[2]
@@ -30,10 +33,11 @@ func main() {
 		color = colorSplit[1]
 		lettersToColor = inputString
 		if len(colorSplit) != 2 || colorSplit[0] != "--color" {
-			fmt.Println("Usage: go run . --color=<color> <letters to be colored> \"something\"")
+			fmt.Println("Usage: go run . [OPTION] [STRING] \n\nEX: go run . --color=<color> <letters to be colored> \"something\"")
 			return
 		}
 	}
+	// case color selected , letters to be colored specified and input string
 	if len(os.Args) == 4 {
 		colorFlag := os.Args[1]
 		inputString = os.Args[3]
@@ -41,30 +45,7 @@ func main() {
 		colorSplit := strings.Split(colorFlag, "=")
 		color = colorSplit[1]
 		if len(colorSplit) != 2 || colorSplit[0] != "--color" {
-			fmt.Println("Usage: go run . --color=<color> <letters to be colored> \"something\"")
-			return
-		}
-	}
-
-	if len(os.Args) == 5 {
-		colorFlag := os.Args[1]
-		inputString = os.Args[3]
-		lettersToColor = os.Args[2]
-		colorSplit := strings.Split(colorFlag, "=")
-		color = colorSplit[1]
-		if len(colorSplit) != 2 || colorSplit[0] != "--color" {
-			fmt.Println("Usage: go run . --color=<color> <letters to be colored> \"something\"")
-			return
-		}
-		bannerFlag := os.Args[4]
-		if bannerFlag == "-shadow" {
-			filePath = "../bannerfiles/shadow.txt"
-		} else if bannerFlag == "-thinkertoy" {
-			filePath = "../bannerfiles/thinkertoy.txt"
-		} else if bannerFlag == "-standard" {
-			filePath = "../bannerfiles/standard.txt"
-		} else {
-			fmt.Println("Please provide the correct flag, <-shadow> or <-thinkertoy> or <standard>")
+			fmt.Println("Usage: go run . [OPTION] [STRING] \n\nEX: go run . --color=<color> <letters to be colored> \"something\"")
 			return
 		}
 	}
@@ -72,7 +53,7 @@ func main() {
 	// Extract the letters to be colored and the rest of the string
 	// lettersToColor, inputString = extractLettersToColor(args)
 
-	characterMap, err := asciiart.CreateMap(filePath)
+	characterMap, err := asciiart.CreateMap("../bannerfiles/standard.txt")
 	if err != nil {
 		fmt.Println("Error reading map:", err)
 		return
