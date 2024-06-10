e093c0895aab962544a5e05302d7e400b8c642fc
shfana <wambitafana@gmail.com>
2024-06-10 14:25:53 +0300

    fix: change variable names

--- colorconversions/rgbtoansi.go
+++ colorconversions/rgbtoansi.go
@@ -4,16 +4,9 @@ import "fmt"
 
 // convert the normalised rgb values to the nearest ansi 256 color code
 func ConvertRGBToAnsi(r, g, b int) string {
-<<<<<<< HEAD
-	rnorm := r / 51
-	gnorm := g / 51
-	bnorm := b / 51
-	color := 16 + ((36 * rnorm) + (6 * gnorm) + bnorm)
-=======
 	rnormal := r / 51
 	gnormal := g / 51
 	bnormal := b / 51
 	color := 16 + ((36 * rnormal) + (6 * gnormal) + bnormal)
->>>>>>> 55cb84d (fix: chnage variable names)
 	return fmt.Sprintf("\033[38;5;%dm", color)
 }
