e379245ce72e3b2c5f24c28b6ecc632f7efeedbc
shfana <wambitafana@gmail.com>
2024-06-11 12:00:59 +0300

    fix:error messgaes fix:case string+banner

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	// check length of arguments
 	if len(os.Args) == 1 || len(os.Args) > 5 {
-		fmt.Println("Usage: go run . [OPTION] and/or [BANNER] [STRING] \n\nEX: go run . --color=<color> <letters to be colored> \"something\"")
+		fmt.Println("Usage: go run . [OPTION] [STRING] \n\nEX: go run . --color=<color> <letters to be colored> \"something\"")
 		return
 	}
 	var (
@@ -29,28 +29,27 @@ func main() {
 			color = "resetCode"
 			fileName = "standard.txt"
 		} else {
-			fmt.Println("Usage: go run . [OPTION] and/or [BANNER] [STRING] \n\nEX: go run . --color=<color> <letters to be colored> \"something\"")
+			fmt.Println("Usage: go run . [OPTION] [STRING] \n\nEX: go run . --color=<color> <letters to be colored> \"something\"")
 			return
 		}
 	}
 	// case color selected but no specified letters to be colored (color+string)
 	if len(os.Args) == 3 {
-		if strings.HasPrefix(os.Args[1], "--color=") {
-			colorFlag := os.Args[1]
-			inputString = os.Args[2]
-			fileName = "standard.txt"
-			if !(strings.HasPrefix(colorFlag, "--color=")) {
-				fmt.Println("Usage: go run . [OPTION] [STRING] \n\nEX: go run . --color=<color> <letters to be colored> \"something\"")
-				return
-			}
-			colorSplit := strings.Split(colorFlag, "=")
-			color = colorSplit[1]
-			lettersToColor = inputString
-			if len(colorSplit) != 2 || colorSplit[0] != "--color" {
-				fmt.Println("Usage: go run . [OPTION] [STRING] \n\nEX: go run . --color=<color> <letters to be colored> \"something\"")
-				return
-			}
+		colorFlag := os.Args[1]
+		inputString = os.Args[2]
+		fileName = "standard.txt"
+		if !(strings.HasPrefix(colorFlag, "--color=")) {
+			fmt.Println("Usage: go run . [OPTION] [STRING] \n\nEX: go run . --color=<color> <letters to be colored> \"something\"")
+			return
 		}
+		colorSplit := strings.Split(colorFlag, "=")
+		color = colorSplit[1]
+		lettersToColor = inputString
+		if len(colorSplit) != 2 || colorSplit[0] != "--color" {
+			fmt.Println("Usage: go run . [OPTION] [STRING] \n\nEX: go run . --color=<color> <letters to be colored> \"something\"")
+			return
+		}
+
 	}
 	// case color selected , letters to be colored specified and input string (color+letter+string)
 	if len(os.Args) == 4 {
@@ -70,7 +69,7 @@ func main() {
 		}
 	}
 	// case string+banner
-	if len(os.Args) == 3 && ((os.Args[2] == "shadow") || (os.Args[2] == "thinkertoy") || (os.Args[2] == "standard")) {
+	if len(os.Args) == 3 && ((os.Args[2] == "shadow") || (os.Args[2] == "thinkertoy") || (os.Args[2] == "standard")) && !(strings.HasPrefix(os.Args[1], "--color=")) {
 		inputString = os.Args[1]
 		lettersToColor = inputString
 		color = "resetCode"
